18398f8f3119717a67a5f2e6edfa72d72f0683cb
Yattaze <yattaze@yattaze.fritz.box>
2017-09-04 18:45:51 +1200

    Added Automated Testing

--- hfc.py
+++ hfc.py
@@ -1,9 +1,15 @@
-import os, sys
+import os, sys, argparse
 from cmd import Cmd
 from importlib import import_module
 from termcolor import colored
 
 sys.dont_write_bytecode = True
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--tests", help="run automated tests",
+                    action="store_true")
+args = parser.parse_args()
+
 
 scanners = sum([len(files) for r, d, files in os.walk('modules/scanners')])
 exploits = sum([len(files) for r, d, files in os.walk('modules/exploits')])
@@ -61,11 +67,41 @@ class hfc(Cmd):
         """Exits hfc"""
         exit(0)
 
+def runTests():
+    h = hfc('')
+    h.do_help('')
+    h.do_show('')
+    h.do_exploit('')
+    h.do_set('')
+
+    h.do_use('misc.ssh.bannergrab')
+    h.do_show('')
+    h.do_exploit('')
+    h.do_set("rhost '144.76.78.177'")
+    h.do_exploit('')
+
+    h.do_use('scanners.http.apachestruts.2017_5638')
+    h.do_show('')
+    h.do_exploit('')
+    h.do_set("rhost 'http://struts.jgeppert.com/struts2-bootstrap-showcase/index.action'")
+    h.do_exploit('')
+
+    h.do_use('exploits.http.apachestruts.2017_5638_Shell')
+    h.do_exploit('')
+    h.do_set("rhost 'http://struts.jgeppert.com/struts2-bootstrap-showcase/index.action'")
+    h.do_set("testing True")
+    h.do_exploit('')
+
+
 if __name__ == "__main__":
     print('''
         Hellion Framework Console
     [Scanners: '''+str(scanners)+''', Exploits: '''+str(exploits)+''', Misc: '''+str(misc)+''']\n\n\n
     ''')
+    if(args.tests):
+        runTests()
     prompt = hfc()
     prompt.prompt = 'hfc> '
     prompt.cmdloop('Starting hfc...')
+
+
